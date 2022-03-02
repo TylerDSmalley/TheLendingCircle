3c5367ae343f7a14350b599fa78676ce084f801c
Tyler <TylerDSmalley@gmail.com>
2022-03-02 12:30:32 -0500

    Initial commit  - TheLendingCircle

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace TheLendingCircle.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+    
+    public override DbSet<IdentityUser> Users { get; set; }
+
+    //Probably can be removed once we migrate to hosted DB
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //TODO: Add SqlServer data source here
+            optionsBuilder.UseSqlite(@"Data source=TheLendingCircleDb.sqlite");
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TheLendingCircle.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//Local DB version
+builder.Services.AddDbContext<ApplicationDbContext>();
+//Hosted DB version
+// builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//     options.UseSqlServer(connectionString));builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//     options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+//Set to true if we implement email confirmation
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    // Cookie settings
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.SlidingExpiration = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+var userManager = builder.Services.BuildServiceProvider().GetService<UserManager<IdentityUser>>();
+var roleManager = builder.Services.BuildServiceProvider().GetService<RoleManager<IdentityRole>>();
+
+SeedUsersAndRoles(userManager, roleManager);
+
+void SeedUsersAndRoles(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager) {
+    string[] roleNamesList = new string[] { "User", "Admin" };
+    foreach (string roleName in roleNamesList) {
+        if(!roleManager.RoleExistsAsync(roleName).Result) {
+            IdentityRole role = new IdentityRole();
+            role.Name = roleName;
+            IdentityResult result = roleManager.CreateAsync(role).Result;
+        }
+    }
+
+    string adminEmail = "admin@admin.com";
+    string adminPass = "Password1";
+    if (userManager.FindByNameAsync(adminEmail).Result == null) {
+        IdentityUser user = new IdentityUser();
+        user.UserName = adminEmail;
+        user.Email = adminEmail;
+        user.EmailConfirmed = true;
+        IdentityResult result = userManager.CreateAsync(user, adminPass).Result;
+        
+        if (result.Succeeded) {
+            IdentityResult result1 = userManager.AddToRoleAsync(user, "Admin").Result;
+            if(result1.Succeeded) {
+                userManager.AddToRoleAsync(user, "Admin").Wait();
+            } else {
+                //log error
+            }
+            
+        }
+    }
+}
+
+app.MapRazorPages();
+
+app.Run();
