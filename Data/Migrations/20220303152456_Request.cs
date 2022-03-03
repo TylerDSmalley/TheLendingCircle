08e34699fb00c4e6fafe89aa3ef6f1ffcc20ed19
Tyler <TylerDSmalley@gmail.com>
2022-03-03 10:28:30 -0500

    New tables
    Added Review and Request tables

---
+++ Data/Migrations/20220303152456_Request.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheLendingCircle.Data.Migrations
+{
+    public partial class Request : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
