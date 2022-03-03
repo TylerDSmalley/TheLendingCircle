18d5d8fc439a9c984fa9ec8f15b6bb6b3228a190
Tyler <TylerDSmalley@gmail.com>
2022-03-03 18:30:33 -0500

    Batch Update
    My Circle (My Items) template created

---
+++ Pages/User/MyCircle.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheLendingCircle.Pages.User
+{
+    public class MyCircleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
