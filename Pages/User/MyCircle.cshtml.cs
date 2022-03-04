e8b8986a8ddbb12e6b369c309330f563dd5aa191
Tyler <TylerDSmalley@gmail.com>
2022-03-03 23:49:37 -0500

    Batch update
    -Added logic to MyCircle Page to display a list of the logged in user's items
    -Styled the page to be flexible with the dynamic content
    -Added button that links to Add Item page.

--- Pages/User/MyCircle.cshtml.cs
+++ Pages/User/MyCircle.cshtml.cs
@@ -1,12 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TheLendingCircle.Models;
 
 namespace TheLendingCircle.Pages.User
 {
+    [Authorize]
     public class MyCircleModel : PageModel
     {
-        public void OnGet()
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly TheLendingCircle.Data.ApplicationDbContext _context;
+
+        public MyCircleModel(UserManager<ApplicationUser> userManager,
+            TheLendingCircle.Data.ApplicationDbContext context)
         {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public ApplicationUser CurrentUser { get; set; }
+
+        public List<Item> UserItems { get; set; }
+
+        private async Task LoadAsync(string id)
+        {
+            UserItems = await _context.Items.Where(i => i.Owner.Id == id).ToListAsync();
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            CurrentUser = user;
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            await LoadAsync(user.Id);
+            return Page();
         }
     }
 }
