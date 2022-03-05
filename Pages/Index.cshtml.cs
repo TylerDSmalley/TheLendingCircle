d6e88dffb2ef7ed40492ff7da3be8e71114351c6
PhilArceno <54872569+PhilArceno@users.noreply.github.com>
2022-03-05 01:00:15 -0500

    myitems (needs fixing)

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,17 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TheLendingCircle.Models;
+using TheLendingCircle.Data;
 
 namespace TheLendingCircle.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly ApplicationDbContext _context;
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public List<Item> ItemsList {get;set;} = new List<Item>();
+    public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
     public void OnGet()
     {
+        ItemsList = _context.Items.Take(6).ToList();
     }
 }
--- Pages/Items/Index.cshtml.cs
+++ Pages/Items/Index.cshtml.cs
@@ -5,20 +5,40 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TheLendingCircle.Models;
+using TheLendingCircle.Data;
 
 namespace TheLendingCircle.Pages.Items
 {
     public class Index : PageModel
     {
+        private ApplicationDbContext _context;
         private readonly ILogger<Index> _logger;
 
-        public Index(ILogger<Index> logger)
+        [BindProperty(SupportsGet = true)]
+        public int Id { get; set; }
+        public Item? CurrentItem { get; set; }
+        // public ApplicationUser? Owner {get;set;}
+
+        public Index(ILogger<Index> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            var user = _context.Users.First(i => i.Email == "jim@bob.com");
+            var item = _context.Items.First(i => i.Id == Id);
+            if (item == null)
+            {
+                return NotFound($"Unable to item user with ID '{Id}'.");
+            }
+            CurrentItem = item;
+            _logger.LogInformation($"Owner: {CurrentItem.Owner}");
+
+            // Owner = await _context.Users.FindAsync(CurrentItem.Owner.Id);
+            return Page();
         }
     }
 }
