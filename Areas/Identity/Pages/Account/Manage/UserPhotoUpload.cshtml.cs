3448c92c62e786db873fe327649100026ebc51c9
Tyler <TylerDSmalley@gmail.com>
2022-03-06 12:46:34 -0500

    Batch update
    Photo upload form created with initial logic

---
+++ Areas/Identity/Pages/Account/Manage/UserPhotoUpload.cshtml.cs
@@ -0,0 +1,52 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TheLendingCircle.Models;
+
+namespace TheLendingCircle.Areas.Identity.Pages.Account.Manage
+{
+    [Authorize]
+    public class UserPhotoUploadModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly TheLendingCircle.Data.ApplicationDbContext _context;
+        public UserPhotoUploadModel(
+            UserManager<ApplicationUser> userManager,
+            TheLendingCircle.Data.ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            //Maybe filesize limit annotation
+            public IFormFile userPhoto { get; set; }
+        }
+
+        public ApplicationUser CurrentUser { get; set; }
+
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            CurrentUser = await _userManager.GetUserAsync(User);
+            if (CurrentUser == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            return Page();
+        }
+
+        // public async Task<IActionResult> OnPostAsync(int? id)
+        // {
+
+        // }
+    }
+}
