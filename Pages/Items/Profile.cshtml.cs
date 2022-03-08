4d7ac34c86c1c104e6e7e3896098cfaed588cb0d
Ileana528 <86622807+Ileana528@users.noreply.github.com>
2022-03-08 13:13:41 -0500

    Added to Profile code behind

--- Pages/Items/Profile.cshtml.cs
+++ Pages/Items/Profile.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TheLendingCircle.Models;
 
 namespace TheLendingCircle.Pages
 {
+    
     public class ProfileModel : PageModel
     {
-        public void OnGet()
+        private readonly TheLendingCircle.Data.ApplicationDbContext _context;
+
+        public ProfileModel(TheLendingCircle.Data.ApplicationDbContext context){
+            _context = context;
+        }
+
+        public ApplicationUser? curUser {get; set;}
+        public List<Review> userReviews {get; set;}
+       public async Task<IActionResult> OnGetAsync(string? id)
         {
+             curUser = await _context.Users.FirstOrDefaultAsync(e => e.Email == id);
+             userReviews = await _context.Review.Where(i => i.Borrower.Id == curUser.Id).ToListAsync();
+            
+            if (curUser == null)
+            {
+                return NotFound($"Unable to load user");
+            }
+            return Page();
         }
     }
 }
