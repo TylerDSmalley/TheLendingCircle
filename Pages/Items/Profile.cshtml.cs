614100d7ea39fe5228ebc8fae5832612e3262289
Ileana528 <86622807+Ileana528@users.noreply.github.com>
2022-03-08 10:28:37 -0500

    Created Profile code behind

---
+++ Pages/Items/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheLendingCircle.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
